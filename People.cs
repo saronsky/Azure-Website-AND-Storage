ef1ecae7b093fc5f04bb1bf7d14edc2aa90af6df
saronsky <59819108+saronsky@users.noreply.github.com>
2021-03-28 11:55:22 -0400

    Add files via upload

---
+++ People.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Program_3_Website_Storage.Models
+{
+    public class ViewModel
+    {
+        public Person person { get; set; }
+        public List<Person> people { get; set; }
+        public string message { get; set; } = "";
+    }
+    public class Person
+    {
+        public string PartitionKey { get; set; }
+
+        public string RowKey { get; set; }
+
+        public IDictionary<string, EntityProperty> attributes = new Dictionary<string, EntityProperty>();
+
+    }
+
+    public class People 
+    {
+        public List<Person> people = new List<Person>();
+
+        public string fixSpaces(string attributeString)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i=0; i<attributeString.Length; i++)
+            {
+                if (i == 0 || !(attributeString[i] == ' ' && attributeString[i - 1] == ' '))
+                    sb.Append(attributeString[i]);
+            }
+            return sb.ToString();
+        }
+        public void setAttributes(string attributeString)
+        {
+            attributeString = fixSpaces(attributeString);
+            if (attributeString.EndsWith("\n"))
+                attributeString = attributeString.Remove(attributeString.Length - 1, 1);
+            string[] peopleAttributes = attributeString.Split('\n');
+            foreach (string personWhole in peopleAttributes)
+            {
+                if (personWhole != "") {
+                    string personWholeFixed = personWhole;
+                    if (personWholeFixed.EndsWith("\r"))
+                        personWholeFixed = personWholeFixed.Remove(personWholeFixed.Length - 1, 1);
+                    string[] personBroken = personWholeFixed.Split(null);
+                    Person tempPerson = new Person();
+                    tempPerson.RowKey = personBroken[0];
+                    tempPerson.PartitionKey = personBroken[1];
+                    for (int i = 2; i < personBroken.Length; i++)
+                    {
+                        string[] attribute = personBroken[i].Split('=');
+                        //this was done because "id" is a reserved keyword in azure, and queries are case insensitive. So the id property, if left as is, would not be added
+                        if (attribute[0] == "id")
+                            attribute[0] = "_id";
+                        tempPerson.attributes.Add(attribute[0], new EntityProperty(attribute[1]));
+                    }
+                    people.Add(tempPerson);
+                }
+                
+            }
+        }
+    }
+}
---
+++ PeopleController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Program_3_Website_Storage.Models;
+using System.Web.Mvc;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.Cosmos.Table;
+using System.IO;
+using System.Text;
+using CloudTable = Microsoft.Azure.Cosmos.Table.CloudTable;
+using System.Configuration;
+
+namespace Program_3_Website_Storage.Controllers
+{
+    public class PeopleController : Controller
+    {
+        static string blobConnectionString = ConfigurationManager.ConnectionStrings["blobConnectionString"].ConnectionString;
+        static string tableConnectionString = ConfigurationManager.ConnectionStrings["tableConnectionString"].ConnectionString;
+        static string tableURL = "https://simon.table.cosmos.azure.com";
+        static string peopleURL = "https://css490.blob.core.windows.net/lab4/input.txt";
+        static BlobServiceClient blobClient = new BlobServiceClient(blobConnectionString);
+        static string containerName = "program4container";
+        static string blobName = "peopleblob";
+        static BlobContainerClient container = new BlobContainerClient(blobConnectionString, containerName);
+        static CloudStorageAccount storageAccount = Microsoft.Azure.Cosmos.Table.CloudStorageAccount.Parse(tableConnectionString);
+        static CloudTableClient cloudTableClient = storageAccount.CreateCloudTableClient();
+        static CloudTable table = cloudTableClient.GetTableReference("peopletable");
+
+        public ActionResult PersonSearch()
+        {
+            ViewBag.Title = "Search Page";
+
+            return View();
+        }
+        public ActionResult handleLoad(ViewModel model)
+        {
+            getPeople();
+            model.message = "Database Loaded";
+            return View("PersonSearch", model);
+        }
+
+        public ActionResult handleClear(ViewModel model)
+        {
+            table.DeleteIfExists();
+            container.GetBlobClient(blobName).DeleteIfExists();
+            model.message = "Database Cleared";
+            return View("PersonSearch", model);
+        }
+
+        public ActionResult handleQuery(ViewModel model)
+        {
+            List<DynamicTableEntity> query;
+            if (model.person.PartitionKey == null)
+            {
+                query = table.CreateQuery<DynamicTableEntity>().Where(x => x.RowKey == model.person.RowKey).ToList();
+            }
+            else if(model.person.RowKey==null)
+            {
+                query = table.CreateQuery<DynamicTableEntity>().Where(x => x.PartitionKey == model.person.PartitionKey).ToList();
+            }
+            else
+            {
+                query = table.CreateQuery<DynamicTableEntity>().Where(x => x.PartitionKey==model.person.PartitionKey && x.RowKey == model.person.RowKey).ToList();
+            }
+
+
+            List<Person> people = new List<Person>();
+            foreach (DynamicTableEntity result in query)
+            {
+                Person person = new Person();
+                person.PartitionKey = result.PartitionKey;
+                person.RowKey = result.RowKey;
+                person.attributes = result.Properties;
+                people.Add(person);
+            }
+            model.people = people;
+            model.message = "Database Queried";
+            return View("PersonSearch", model);
+        }
+
+
+        static void handleBlob(string text)
+        {
+            var content = Encoding.UTF8.GetBytes(text);
+            using (var ms = new MemoryStream(content))
+                container.GetBlobClient(blobName).Upload(ms, overwrite: true);
+        }
+        
+        static void handleTable(string text)
+        {
+            table.CreateIfNotExists();
+            People people = new People();
+            people.setAttributes(text);
+            
+            foreach(Person person in people.people)
+            {
+                var dte = new DynamicTableEntity(person.PartitionKey, person.RowKey);
+                dte.Properties = person.attributes;
+                table.Execute(TableOperation.InsertOrReplace(dte));
+            }
+        }
+        static Boolean getPeople()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(peopleURL);
+            string result = handleResponse(client, -1);
+            if (result.Length != 0)
+            {
+                handleBlob(result);
+                handleTable(result);
+                return true;
+            }
+            return false;
+        }
+        static string handleResponse(HttpClient client, int retry)
+        {
+            if (retry > 0)
+                System.Threading.Thread.Sleep(retry * 1000);
+            HttpResponseMessage response = client.GetAsync("").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                int statusCode = (int)response.StatusCode;
+                if (statusCode / 100 == 5 && retry < 8)
+                {
+                    if (retry < 1)
+                        retry++;
+                    else
+                        retry *= 2;
+                    return handleResponse(client, retry);
+                }
+                Console.WriteLine("API request failed. Status code: " + response.StatusCode);
+                return "";
+            }
+            return response.Content.ReadAsStringAsync().Result;
+        }
+    }
+
+    
+
+}
